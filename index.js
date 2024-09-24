9c19c5481d30b98f0674e2107bd8894ba1fca0fb
minhazapon <minhajulabedinapon@gmail.com>
2024-09-23 22:24:09 -0700

    added file

--- index.js
+++ index.js
@@ -17,10 +17,29 @@ app.get('/', (req, res) => {
 })
 
 
+const breads = [
+
+    {id:1, img:'https://assets.nicepagecdn.com/d2cc3eaa/3379718/images/ghgh.jpg', name:'White breads', details:'this companies breads are so good and premium plzzz take this companies products' },
+    {id:2, img:'https://assets.nicepagecdn.com/d2cc3eaa/3379718/images/rre.jpg', name:'White breads', details:'this companies breads are so good and premium plzzz take this companies products' },
+    {id:3, img:'https://assets.nicepagecdn.com/d2cc3eaa/3379718/images/trrt.jpg', name:'White breads', details:'this companies breads are so good and premium plzzz take this companies products' },
+    {id:4, img:'https://assets.nicepagecdn.com/d2cc3eaa/3379718/images/j.jpg', name:'White breads', details:'this companies breads are so good and premium plzzz take this companies products' },
+    {id:5, img:'https://assets.nicepagecdn.com/d2cc3eaa/3379718/images/666662.jpg', name:'White breads', details:'this companies breads are so good and premium plzzz take this companies products' },
+    {id:6, img:'https://assets.nicepagecdn.com/d2cc3eaa/3379718/images/fgfgfg.jpg', name:'White breads', details:'this companies breads are so good and premium plzzz take this companies products' },
+    {id:7, img:'https://assets.nicepagecdn.com/d2cc3eaa/3379718/images/gfgffg.jpg', name:'White breads', details:'this companies breads are so good and premium plzzz take this companies products' },
+    {id:8, img:'https://assets.nicepagecdn.com/d2cc3eaa/3379718/images/fgfgr.jpg', name:'White breads', details:'this companies breads are so good and premium plzzz take this companies products' }
+
+
+]
+
+app.get('/breads', (req, res) => {
+  res.send(breads)
+})
+
+
 ////////////////////////////////////////mongoDB/////////////////////////////
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruz4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -42,23 +61,91 @@ async function run() {
 
     //crud//
 
+   
+
+    
+    //create//
+
     app.post('/users',  async(req, res) => {
 
-        const users = req.body 
-        console.log(users) 
-        const result = await userCollection.insertOne(users)
-        console.log(result)
-        res.send(result)
-    
+      const users = req.body 
+      console.log(users) 
+      const result = await userCollection.insertOne(users)
+      res.send(result)
+  
+    }) 
+    //create//
+
+    //read// 
+
+    app.get('/users', async(req, res) => {
+     
+         const cursor = userCollection.find();
+         const result = await cursor.toArray();
+         res.send(result)
+ 
+
     })
+    //read//
 
+
+    //delete//
+      
+    app.delete('/users/:id', async(req, res) => {
+     
+       const id = req.params.id 
+       const query = { _id: new ObjectId(id)}
+       const result = await userCollection.deleteOne(query)
+       res.send(result)
+
+    })
+    //delete//
+
+
+    //update// 
     
-    //create//
-
     
+    app.get('/users/:id', async(req, res) => {
+     
+      const id = req.params.id 
+      const query = { _id: new ObjectId(id)}
+      const result = await userCollection.findOne(query)
+      res.send(result)
 
+   })
 
-    //create//
+  
+    app.put('/users/:id', async(req, res) => {
+     
+      const id = req.params.id 
+      const upUser = req.body 
+      console.log(id, upUser)
+      const filter = { _id: new ObjectId(id)} 
+      const option = {upsert: true}
+      const upSer = {
+        
+        $set: {
+          name: upUser.name,
+          Chef: upUser.Chef,
+          Supplier: upUser.Supplier,
+          PhotoUrl: upUser.PhotoUrl,
+          Taste: upUser.Taste,
+          Category: upUser.Category,
+          Details: upUser.Details
+        }   
+ 
+      }  
+ 
+      const result = await userCollection.updateOne(filter, option, upSer)
+      res.send(result)
+
+ 
+
+   })
+ 
+ 
+ 
+    //update//
 
 
 
